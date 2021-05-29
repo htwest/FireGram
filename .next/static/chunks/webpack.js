29b854803d74addab010d4e4de64fca363cebc07
Hayden West <haydenw7@gmail.com>
2021-05-28 17:26:57 -0700

    rendered images from retrived urls

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "029eba84c8b36332e2d8"; }
+/******/ 		__webpack_require__.h = function() { return "9b7dd72d8581783e9af3"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- comps/ImageGrid.js
+++ comps/ImageGrid.js
@@ -1,10 +1,21 @@
 import useFirestore from "../hooks/useFirestore";
 
 const ImageGrid = () => {
+  // Retrieves documents from "images" collection in firestoreDB
   const { docs } = useFirestore("images");
-  console.log(docs);
+  // console.log(docs);
 
-  return <div className="img-grid">images</div>;
+  return (
+    <div className="img-grid">
+      {docs
+        ? docs.map((doc) => (
+            <div className="img-wrap" key={doc.id}>
+              <img src={doc.url} alt="Image" />
+            </div>
+          ))
+        : null}
+    </div>
+  );
 };
 
 export default ImageGrid;
